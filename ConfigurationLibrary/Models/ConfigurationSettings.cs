69fa6a30123055b395e0a1fc2ec96df950db31d0
Selcuk <Selcuk@DESKTOP-RVSKJ2F>
2025-03-13 11:46:05 +0300

    Add project files.

---
+++ ConfigurationLibrary/Models/ConfigurationSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfigurationLibrary.Models
+{
+    class ConfigurationSettings
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Value { get; set; }
+        public bool IsActive { get; set; }
+        public string ApplicationName { get; set; }
+    }
+}
